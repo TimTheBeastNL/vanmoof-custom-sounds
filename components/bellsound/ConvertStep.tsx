c59dfe2ecbb4c2521791587f4fb9cdbc6a74724c
Tim Kunst <118755239+TimTheBeastNL@users.noreply.github.com>
2024-06-25 22:30:32 +0200

    Update ConvertStep.tsx

--- components/bellsound/ConvertStep.tsx
+++ components/bellsound/ConvertStep.tsx
@@ -93,7 +93,8 @@ export default function ConvertStep({ bike, onDismiss, selectedFile, onConversio
     const startConversion = async () => {
         if (converting) return
         setConverting(true)
-        await bike.initiateBellSoundTransfer(selectedFile.arrayBuffer())
+        const arrayBuffer = await selectedFile.arrayBuffer(); // Await the promise to resolve
+        await bike.initiateBellSoundTransfer(arrayBuffer);
         log("Choosing appropriate sample rate...")
         const audioContext = new AudioContext()
         const audioBuffer = await audioContext.decodeAudioData(await selectedFile.arrayBuffer())
